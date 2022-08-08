678716fdce2f1d0d6497c6123298d589815c04f6
SL_JaNa <93564410+SLJaNa@users.noreply.github.com>
2022-08-08 20:11:55 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Owner", "https://www.github.com/anasty17")
+    buttons.buildbutton("Repo", "https://github.com/SLJaNa/SL_Jana_Telegram-Leecher_Bot")
+    buttons.buildbutton("Owner", "https://github.com/SLJaNa")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -174,7 +174,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Mirror-Leech-Bot Help',
+        title='SL_Jana_Telegram-Leecher_Bot Help',
         content=help_string_telegraph,
     )["path"]
 
